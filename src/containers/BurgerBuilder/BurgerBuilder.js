6b0a395558463744fa1009f3bc1431c45804ffa0
Gavin Quirk <gavinquirk@gmail.com>
2018-12-31 16:31:09 -0800

    added controls to component, handle add ingredient method and button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,23 +1,54 @@
 import React, { Component } from 'react'
 
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
 
   state = {
     ingredients: {
-      salad: 0,
+      salad: 1,
       bacon: 0,
       cheese: 0,
       meat: 0
+    },
+    totalPrice: 4
+  }
+
+  addIngredientHandler = (type) => {
+    // Handling of ingredient type count
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = oldCount + 1
+    const updatedIngredients = {
+      ...this.state.ingredients
     }
+    updatedIngredients[type] = updatedCount
+    // Handling of price updates
+    const priceAddition = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice + priceAddition
+    // Update ingredients and price
+    this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
+  }
+
+  removeIngredientHandler = (type) => {
+
   }
 
   render() {
     return (
       <>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+        />
       </>
     )
   }
