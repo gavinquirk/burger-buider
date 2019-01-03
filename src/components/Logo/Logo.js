0fb3e74b4626e127ebded634b6fc2eb67ba032d7
Gavin Quirk <gavinquirk@gmail.com>
2019-01-03 00:40:46 -0800

    toolbar logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './Logo.css'
+
+import burgerLogo from '../../assets/images/burger-logo.png'
+
+const logo = (props) => (
+  <div className={classes.Logo}>
+    <img src={burgerLogo} alt="Burger Builder Logo"/>
+  </div>
+)
+
+export default logo
