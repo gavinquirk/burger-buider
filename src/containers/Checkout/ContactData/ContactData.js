e0df1a4ec78c4bc4ea7658feeedc21f842d85ec2
Gavin Quirk <gavinquirk@gmail.com>
2019-01-09 15:14:23 -0800

    validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -21,7 +21,8 @@ class ContactData extends Component {
           validation: {
             required: true
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         street: {
           elementType: 'input',
@@ -33,7 +34,8 @@ class ContactData extends Component {
           validation: {
             required: true
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         zipCode: {
           elementType: 'input',
@@ -47,7 +49,8 @@ class ContactData extends Component {
             minLength: 5,
             maxLength: 5
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         country: {
           elementType: 'input',
@@ -59,7 +62,8 @@ class ContactData extends Component {
           validation: {
             required: true
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         email: {
           elementType: 'input',
@@ -71,7 +75,8 @@ class ContactData extends Component {
           validation: {
             required: true
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         deliveryMethod: {
           elementType: 'select',
@@ -81,9 +86,11 @@ class ContactData extends Component {
               {value: 'cheapest', displayValue: 'Cheapest'}
             ]
           },
-          value: 'fastest'
+          value: 'fastest',
+          valid: true
         }
     },
+    formIsValid: false,
     loading: false
   }
 
@@ -111,6 +118,8 @@ class ContactData extends Component {
 
   checkValidity(value, rules) {
 
+    console.log(rules)
+
     let isValid = true
 
     if (rules.required) { // If element contains validation rules
@@ -129,16 +138,27 @@ class ContactData extends Component {
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
+    // Clone order form to prevent state mutation
     const updatedOrderForm = {
       ...this.state.orderForm
     }
     const updatedFormElement = {
       ...updatedOrderForm[inputIdentifier]
     }
+
     updatedFormElement.value = event.target.value
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+    updatedFormElement.touched = true
     updatedOrderForm[inputIdentifier] = updatedFormElement
-    this.setState({orderForm: updatedOrderForm})
+
+    // Check if all inputs are valid to submit
+    const formIsValid = true
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+    }
+    console.log(formIsValid)
+
+    this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid})
   }
 
   render () {
@@ -158,10 +178,13 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success" clicked={this.orderHandler} >ORDER</Button>
+        <Button disabled={!this.state.formIsValid} btnType="Success" clicked={this.orderHandler} >ORDER</Button>
       </form>
     )
 
