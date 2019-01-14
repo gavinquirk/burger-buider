1f58be4ca6b747cc05c94bd896b15b996f4cb191
Gavin Quirk <gavinquirk@gmail.com>
2019-01-14 14:19:02 -0800

    signup and signin toggle

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
   }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart())
     const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
       returnSecureToken: true
     }
     const API_KEY = process.env.REACT_APP_AUTH_KEY
-    axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + API_KEY, authData)
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + API_KEY
+    if (!isSignup) {
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + API_KEY
+    }
+    axios.post(url, authData)
       .then(response => {
         console.log(response)
         dispatch(authSuccess(response.data))
