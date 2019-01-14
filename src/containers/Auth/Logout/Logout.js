3932bd0efee78ac4b06f8211093576159ad8a986
Gavin Quirk <gavinquirk@gmail.com>
2019-01-14 15:32:56 -0800

    logout redirect

---
+++ src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+import * as actions from '../../../store/actions/index'
+
+// Redirect user to main page upon loading this component
+
+class Logout extends Component {
+
+  componentDidMount() {
+    this.props.onLogout()
+  }
+
+  render () {
+    return <Redirect to="/"/>
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onLogout: () => dispatch(actions.logout)
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Logout)
